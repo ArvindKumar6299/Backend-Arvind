eb42b5b442c1435435a0dab28942132bd192290c
ArvindKumar6299 <arvindkumar735240@gmail.com>
2024-03-01 11:12:53 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
  const asyncHandler = (requestHandler) =>{ 
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promis.resolve(requestHandler(req,res,next))
         .catch((err) => next(err))
     }
